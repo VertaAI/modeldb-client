22e91a9597ecbfeec169a49b0f2b4df9a9a5702c
Conrado Miranda <miranda.conrado@gmail.com>
2019-03-23 14:24:08 -0700

    Fixed case where 'host' argument contains ports or protocols

--- verta/verta/modeldbclient.py
+++ verta/verta/modeldbclient.py
@@ -1,6 +1,7 @@
 import re
 import ast
 import time
+from urllib.parse import urlparse
 
 import requests
 
@@ -52,6 +53,20 @@ class ModelDBClient:
                           self._GRPC_PREFIX+'source': "PythonClient"}
         else:
             raise ValueError("`email` and `dev_key` must be provided together")
+
+        host = urlparse(host)
+        if host.netloc == '':
+            # We passed a host that cannot be resolved into a basic URL. Assume it's the right path
+            host = host.path
+        else:
+            # Otherwise, just get the netlocation, which contains the hostname and port
+            # TODO(conrado): support subpaths? (e.g. example.com/backend)
+            host = host.netloc
+
+        m = re.compile('.*:[0-9]+').match(host)
+        if m:
+            raise ValueError("Argument 'host' already contains a port. Please split and provide as "
+                             "separate arguments.")
 
         self._socket = "{}:{}".format(host, port)
 
