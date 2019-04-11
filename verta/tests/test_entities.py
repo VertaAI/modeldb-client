18feffe3f736ea6a39010521f6fd66ed4f159149
Michael Liu <miliu@protonmail.com>
2019-04-11 16:42:39 -0700

    Create tests for ExptRuns querying

--- verta/tests/test_entities.py
+++ verta/tests/test_entities.py
@@ -1,4 +1,5 @@
 import itertools
+import random
 
 import pytest
 import utils
@@ -35,25 +36,39 @@ class TestExperiment:
 
 
 class TestExperimentRuns:
-    def test_magic_methods(self, client):
+    def test_magic_getitem(self, client):
         client.set_project()
-        expt1 = client.set_experiment()
+        expt = client.set_experiment()
+        expt_run_ids = []
 
-        # __getitem__
-        expt1_run_ids = [client.set_experiment_run()._id for _ in range(12)]
-        for expt_run_id, expt_run in zip(expt1_run_ids, expt1.expt_runs):
+        expt_run_ids.extend(client.set_experiment_run()._id for _ in range(12))
+        for expt_run_id, expt_run in zip(expt_run_ids, expt.expt_runs):
             assert expt_run_id == expt_run._id
 
-        # __len__
-        assert len(expt1_run_ids) == len(expt1.expt_runs)
+        expt_run_ids.extend(client.set_experiment_run()._id for _ in range(12))
+        for expt_run_id, expt_run in zip(expt_run_ids, expt.expt_runs):
+            assert expt_run_id == expt_run._id
 
-        # __add__
+    def test_magic_len(self, client):
+        client.set_project()
+        expt = client.set_experiment()
+        expt_run_ids = [client.set_experiment_run()._id for _ in range(12)]
+
+        assert len(expt_run_ids) == len(expt.expt_runs)
+
+    def test_magic_add(self, client):
+        client.set_project()
+        expt1 = client.set_experiment()
+        expt1_run_ids = [client.set_experiment_run()._id for _ in range(12)]
         expt2 = client.set_experiment()
         expt2_run_ids = [client.set_experiment_run()._id for _ in range(12)]
+
+        # simple concatenation
         for expt_run_id, expt_run in zip(expt1_run_ids + expt2_run_ids,
                                          expt1.expt_runs + expt2.expt_runs):
             assert expt_run_id == expt_run._id
 
+<<<<<<< HEAD
 
 class TestExperimentRun:
     def test_set_experiment_run_warning(self, client):
@@ -64,3 +79,92 @@ class TestExperimentRun:
         for kwargs in KWARGS_COMBOS:
             with pytest.warns(UserWarning):
                 client.set_experiment_run(expt_run.name, **kwargs)
+=======
+        # ignore duplicates
+        woven_run_ids = [id_ for pair in zip(expt1_run_ids, expt2_run_ids) for id_ in pair]
+        woven_expt_runs = expt1.expt_runs.__class__(client._auth, client._socket, woven_run_ids)
+        for expt_run_id, expt_run in zip(expt1_run_ids + expt2_run_ids,
+                                         expt1.expt_runs + woven_expt_runs):
+            assert expt_run_id == expt_run._id
+
+    def test_find(self, client):
+        client.set_project()
+        expt = client.set_experiment()
+
+        metric_vals = random.sample(range(36), 12)
+        hyperparam_vals = random.sample(range(36), 12)
+        for metric_val, hyperparam_val in zip(metric_vals, hyperparam_vals):
+            run = client.set_experiment_run()
+            run.log_metric('val', metric_val)
+            run.log_hyperparameter('val', hyperparam_val)
+
+        threshold = random.choice(metric_vals)
+        filtered_run_ids = [run._id for run in expt.expt_runs if run.get_metric('val') >= threshold]
+        for expt_run_id, expt_run in zip(filtered_run_ids,
+                                         expt.expt_runs.find("metrics.val >= {}".format(threshold))):
+            assert expt_run_id == expt_run._id
+
+        threshold = random.choice(hyperparam_vals)
+        filtered_run_ids = [run._id for run in expt.expt_runs if run.get_hyperparameter('val') >= threshold]
+        for expt_run_id, expt_run in zip(filtered_run_ids,
+                                         expt.expt_runs.find("hyperparameters.val >= {}".format(threshold))):
+            assert expt_run_id == expt_run._id
+
+    def test_sort(self, client):
+        client.set_project()
+        expt = client.set_experiment()
+
+        vals = random.sample(range(36), 12)
+        for val in vals:
+            client.set_experiment_run().log_metric('val', val)
+
+        sorted_run_ids = [run._id for run in sorted(expt.expt_runs, key=lambda run: run.get_metric('val'))]
+        for expt_run_id, expt_run in zip(sorted_run_ids, expt.expt_runs.sort("metrics.val")):
+            assert expt_run_id == expt_run._id
+
+    def test_top_k(self, client):
+        client.set_project()
+        expt = client.set_experiment()
+
+        metric_vals = random.sample(range(36), 12)
+        hyperparam_vals = random.sample(range(36), 12)
+        for metric_val, hyperparam_val in zip(metric_vals, hyperparam_vals):
+            run = client.set_experiment_run()
+            run.log_metric('val', metric_val)
+            run.log_hyperparameter('val', hyperparam_val)
+
+        k = random.randrange(12)
+        top_run_ids = [run._id for run in sorted(expt.expt_runs,
+                                                 key=lambda run: run.get_metric('val'), reverse=True)][:k]
+        for expt_run_id, expt_run in zip(top_run_ids, expt.expt_runs.top_k("metrics.val", k)):
+            assert expt_run_id == expt_run._id
+
+        k = random.randrange(12)
+        top_run_ids = [run._id for run in sorted(expt.expt_runs,
+                                                 key=lambda run: run.get_metric('val'), reverse=True)][:k]
+        for expt_run_id, expt_run in zip(top_run_ids, expt.expt_runs.top_k("metrics.val", k)):
+            assert expt_run_id == expt_run._id
+
+    def test_bottom_k(self, client):
+        client.set_project()
+        expt = client.set_experiment()
+
+        metric_vals = random.sample(range(36), 12)
+        hyperparam_vals = random.sample(range(36), 12)
+        for metric_val, hyperparam_val in zip(metric_vals, hyperparam_vals):
+            run = client.set_experiment_run()
+            run.log_metric('val', metric_val)
+            run.log_hyperparameter('val', hyperparam_val)
+
+        k = random.randrange(12)
+        bottom_run_ids = [run._id for run in sorted(expt.expt_runs,
+                                                    key=lambda run: run.get_metric('val'))][:k]
+        for expt_run_id, expt_run in zip(bottom_run_ids, expt.expt_runs.bottom_k("metrics.val", k)):
+            assert expt_run_id == expt_run._id
+
+        k = random.randrange(12)
+        bottom_run_ids = [run._id for run in sorted(expt.expt_runs,
+                                                    key=lambda run: run.get_metric('val'))][:k]
+        for expt_run_id, expt_run in zip(bottom_run_ids, expt.expt_runs.bottom_k("metrics.val", k)):
+            assert expt_run_id == expt_run._id
+>>>>>>> Create tests for ExptRuns querying
