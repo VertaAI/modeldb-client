5b7ff6706f8785e859cc690d69d3869bfafb8282
Michael Liu <miliu@protonmail.com>
2019-04-22 08:34:27 -0700

    Use six's dict items iter

--- verta/tests/test_workflow.py
+++ verta/tests/test_workflow.py
@@ -15,13 +15,13 @@ class TestHyperparameters:
     }
 
     def test_single(self, experiment_run):
-        for key, val in self.hyperparameters.items():
+        for key, val in six.viewitems(self.hyperparameters):
             experiment_run.log_hyperparameter(key, val)
 
         with pytest.raises(KeyError):
             experiment_run.get_hyperparameter(utils.gen_str())
 
-        for key, val in self.hyperparameters.items():
+        for key, val in six.viewitems(self.hyperparameters):
             assert experiment_run.get_hyperparameter(key) == val
 
         assert experiment_run.get_hyperparameters() == self.hyperparameters
@@ -35,7 +35,7 @@ class TestHyperparameters:
         with pytest.raises(KeyError):
             experiment_run.get_hyperparameter(utils.gen_str())
 
-        for key, val in self.hyperparameters.items():
+        for key, val in six.viewitems(self.hyperparameters):
             assert experiment_run.get_hyperparameter(key) == val
 
         assert experiment_run.get_hyperparameters() == self.hyperparameters
@@ -46,7 +46,7 @@ class TestHyperparameters:
         with pytest.raises(KeyError):
             experiment_run.get_hyperparameter(utils.gen_str())
 
-        for key, val in self.hyperparameters.items():
+        for key, val in six.viewitems(self.hyperparameters):
             assert experiment_run.get_hyperparameter(key) == val
 
         assert experiment_run.get_hyperparameters() == self.hyperparameters
@@ -59,13 +59,13 @@ def test_attributes(experiment_run):
         utils.gen_str(): utils.gen_float(),
     }
 
-    for key, val in attributes.items():
+    for key, val in six.viewitems(attributes):
         experiment_run.log_attribute(key, val)
 
     with pytest.raises(KeyError):
         experiment_run.get_attribute(utils.gen_str())
 
-    for key, val in attributes.items():
+    for key, val in six.viewitems(attributes):
         assert experiment_run.get_attribute(key) == val
 
     assert experiment_run.get_attributes() == attributes
@@ -78,13 +78,13 @@ def test_metrics(experiment_run):
         utils.gen_str(): utils.gen_float(),
     }
 
-    for key, val in metrics.items():
+    for key, val in six.viewitems(metrics):
         experiment_run.log_metric(key, val)
 
     with pytest.raises(KeyError):
         experiment_run.get_metric(utils.gen_str())
 
-    for key, val in metrics.items():
+    for key, val in six.viewitems(metrics):
         assert experiment_run.get_metric(key) == val
 
     assert experiment_run.get_metrics() == metrics
@@ -97,14 +97,14 @@ def test_observations(experiment_run):
         utils.gen_str(): [utils.gen_float(), utils.gen_float()],
     }
 
-    for key, vals in observations.items():
+    for key, vals in six.viewitems(observations):
         for val in vals:
             experiment_run.log_observation(key, val)
 
     with pytest.raises(KeyError):
         experiment_run.get_observation(utils.gen_str())
 
-    for key, val in observations.items():
+    for key, val in six.viewitems(observations):
         assert experiment_run.get_observation(key) == val
 
     assert experiment_run.get_observations() == observations
