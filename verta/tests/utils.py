a0840bdcfb9f7f5df76b12a05f3d5dbff3e5223f
Michael Liu <miliu@protonmail.com>
2019-04-25 20:38:56 -0700

    Use client-provided scheme for deletes

--- verta/tests/utils.py
+++ verta/tests/utils.py
@@ -21,30 +21,30 @@ def gen_float(start=1, stop=None):
 
 
 def delete_project(id_, client):
-    response = requests.get("http://{}/v1/experiment/getExperimentsInProject".format(client._socket),
-                            params={'project_id': id_}, headers=client._auth)
+    request_url = "{}://{}/v1/experiment/getExperimentsInProject".format(client._scheme, client._socket)
+    response = requests.get(request_url, params={'project_id': id_}, headers=client._auth)
     response.raise_for_status()
     for experiment in response.json().get('experiments', []):
         delete_experiment(experiment['id'], client)
 
-    response = requests.delete("http://{}/v1/project/deleteProject".format(client._socket),
-                               json={'id': id_}, headers=client._auth)
+    request_url = "{}://{}/v1/project/deleteProject".format(client._scheme, client._socket)
+    response = requests.delete(request_url, json={'id': id_}, headers=client._auth)
     response.raise_for_status()
 
 
 def delete_experiment(id_, client):
-    response = requests.get("http://{}/v1/experiment-run/getExperimentRunsInExperiment".format(client._socket),
-                            params={'experiment_id': id_}, headers=client._auth)
+    request_url = "{}://{}/v1/experiment-run/getExperimentRunsInExperiment".format(client._scheme, client._socket)
+    response = requests.get(request_url, params={'experiment_id': id_}, headers=client._auth)
     response.raise_for_status()
     for experiment_run in response.json().get('experiment_runs', []):
         delete_experiment_run(experiment_run['id'], client)
 
-    response = requests.delete("http://{}/v1/experiment/deleteExperiment".format(client._socket),
-                               json={'id': id_}, headers=client._auth)
+    request_url = "{}://{}/v1/experiment/deleteExperiment".format(client._scheme, client._socket)
+    response = requests.delete(request_url, json={'id': id_}, headers=client._auth)
     response.raise_for_status()
 
 
 def delete_experiment_run(id_, client):
-    response = requests.delete("http://{}/v1/experiment-run/deleteExperimentRun".format(client._socket),
-                               json={'id': id_}, headers=client._auth)
+    request_url = "{}://{}/v1/experiment-run/deleteExperimentRun".format(client._scheme, client._socket)
+    response = requests.delete(request_url, json={'id': id_}, headers=client._auth)
     response.raise_for_status()
