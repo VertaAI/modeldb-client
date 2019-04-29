6e9ea5efef03651496dc8471dc2b3b846b1585c7
Michael Liu <miliu@protonmail.com>
2019-04-29 16:36:02 -0700

    Remove fix-import.sh

--- verta/fix-import.sh
+++
@@ -1,7 +0,0 @@
-declare -a dirnames=("verta/_protos/public/modeldb/")
-
-for dirname in "${dirnames[@]}"; do
-    for filename in "${dirname}*_pb2*"; do
-        sed -i '' 's|^from protos\.|from ...|g' $filename
-    done
-done
