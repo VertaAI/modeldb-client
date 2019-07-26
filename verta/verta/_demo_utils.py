9757e46734c2f4d41ba25c2df623ccc640c531ab
Michael Liu <miliu@protonmail.com>
2019-07-26 10:32:10 -0700

    Remove obsolete input headers

--- verta/verta/_demo_utils.py
+++ verta/verta/_demo_utils.py
@@ -45,7 +45,6 @@ class DeployedModel:
 
         self._token = None
         self._url = None
-        self._headers = None
 
     def __repr__(self):
         return "<Model {}>".format(self._id)
@@ -60,32 +59,10 @@ class DeployedModel:
         else:
             raise RuntimeError("deployment is not ready")
 
-    def _set_headers(self, key="model_api.json"):
-        # get url to get model_api.json from artifact store
-        params = {'id': self._id, 'key': key, 'method': "GET"}
-        response = requests.post("https://{}/v1/experiment-run/getUrlForArtifact".format(self._socket),
-                                 json=params, headers=self._auth)
-        response.raise_for_status()
-
-        # get model_api.json
-        get_artifact_url = response.json()['url']
-        response = requests.get(get_artifact_url)
-        response.raise_for_status()
-        model_api = json.loads(response.content)
-
-        model_api_input = model_api['input']
-
-        if 'fields' not in model_api_input:
-            self._headers = model_api_input['name']
-        else:
-            self._headers = [field['name'] for field in model_api_input['fields']]
-
     def _predict(self, x, return_input_body=False):
         """This is like ``DeployedModel.predict()``, but returns the raw ``Response`` for debugging."""
         if self._token is None or self._url is None:
             self._set_token_and_url()
-        if self._headers is None:
-            self._set_headers()
 
         result = requests.post(self._url,
                                headers={'Access-token': self._token},
@@ -126,8 +103,5 @@ class DeployedModel:
             self._set_token_and_url()  # try refetching prediction token and URL
             response = self._predict(x)
             if not response.ok:
-                self._set_headers()  # try refetching input headers
-                response = self._predict(x)
-                if not response.ok:
-                    return "model is warming up; please wait"
+                return "model is warming up; please wait"
         return response.json()
