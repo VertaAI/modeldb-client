ebdc15a3bd123ebfe11c44d8b5e65d285acb47d1
Michael Liu <miliu@protonmail.com>
2019-08-22 17:53:06 -0700

    Bring back tests

--- verta/tests/test_backend.py
+++ verta/tests/test_backend.py
@@ -6,36 +6,37 @@ import pytest
 import utils
 
 
-# class TestLoad:
-#     @staticmethod
-#     def run_fake_experiment(client):
-#         run = client.set_experiment_run()
+class TestLoad:
+    @staticmethod
+    def run_fake_experiment(client):
+        run = client.set_experiment_run()
 
-#         run.log_attribute("is_test", True)
-#         run.get_attribute("is_test")
+        run.log_attribute("is_test", True)
+        run.get_attribute("is_test")
 
-#         run.log_hyperparameters({
-#             'C': utils.gen_float(),
-#             'solver': utils.gen_str(),
-#             'max_iter': utils.gen_int(),
-#         })
-#         run.get_hyperparameter("C")
-#         run.get_hyperparameter("solver")
-#         run.get_hyperparameter("max_iter")
+        run.log_hyperparameters({
+            'C': utils.gen_float(),
+            'solver': utils.gen_str(),
+            'max_iter': utils.gen_int(),
+        })
+        run.get_hyperparameter("C")
+        run.get_hyperparameter("solver")
+        run.get_hyperparameter("max_iter")
 
-#         run.log_observation("rand_val", utils.gen_float())
-#         run.log_observation("rand_val", utils.gen_float())
-#         run.log_observation("rand_val", utils.gen_float())
-#         run.get_observation("rand_val")
+        run.log_observation("rand_val", utils.gen_float())
+        run.log_observation("rand_val", utils.gen_float())
+        run.log_observation("rand_val", utils.gen_float())
+        run.get_observation("rand_val")
 
-#         run.log_metric("val_acc", utils.gen_float())
-#         run.get_metric("val_acc")
+        run.log_metric("val_acc", utils.gen_float())
+        run.get_metric("val_acc")
 
-#         run.log_artifact("self", run)
-#         run.get_artifact("self")
+        run.log_artifact("self", run)
+        run.get_artifact("self")
 
-#     def test_load(self, client):
-#         client.set_project()
-#         client.set_experiment()
-#         with Pool(36) as pool:
-#             pool.map(self.run_fake_experiment, [client]*180)
+    @pytest.mark.skip(reason="there is an issue serializing the Client for Pool")
+    def test_load(self, client):
+        client.set_project()
+        client.set_experiment()
+        with Pool(36) as pool:
+            pool.map(self.run_fake_experiment, [client]*180)
