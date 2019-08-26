2120baa4a079afd96cf6c432a020a50d496c2bef
Michael Liu <miliu@protonmail.com>
2019-08-26 13:04:21 -0700

    Create input generation fixtures

--- verta/tests/conftest.py
+++ verta/tests/conftest.py
@@ -1,13 +1,18 @@
 import os
 import shutil
+import string
 
 from google.cloud import bigquery
+import numpy as np
 
 from verta import Client
 
 import pytest
 import utils
 
+
+RANDOM_SEED = 0
+INPUT_LENGTH = 12  # length of iterable input fixture
 
 DEFAULT_HOST = None
 DEFAULT_PORT = None
@@ -37,6 +42,131 @@ def email():
 @pytest.fixture(scope='session')
 def dev_key():
     return os.environ.get("VERTA_DEV_KEY", DEFAULT_DEV_KEY)
+
+
+@pytest.fixture
+def nones():
+    return [None]*INPUT_LENGTH
+
+
+@pytest.fixture
+def bools():
+    np.random.seed(RANDOM_SEED)
+    return np.random.randint(0, 2, INPUT_LENGTH).astype(bool).tolist()
+
+
+@pytest.fixture
+def floats():
+    np.random.seed(RANDOM_SEED)
+    return np.linspace(-3**2, 3**3, num=INPUT_LENGTH).tolist()
+
+
+@pytest.fixture
+def ints():
+    np.random.seed(RANDOM_SEED)
+    return np.linspace(-3**4, 3**5, num=INPUT_LENGTH).astype(int).tolist()
+
+
+@pytest.fixture
+def strs():
+    """no duplicates"""
+    np.random.seed(RANDOM_SEED)
+    gen_str = lambda: ''.join(np.random.choice(list(string.ascii_letters), size=INPUT_LENGTH))
+    result = set()
+    while len(result) < INPUT_LENGTH:
+        single_str = gen_str()
+        while single_str in result:
+            single_str = gen_str()
+        else:
+            result.add(single_str)
+    return list(result)
+
+
+@pytest.fixture
+def flat_lists(nones, bools, floats, ints, strs):
+    np.random.seed(RANDOM_SEED)
+    values = (nones, bools, floats, ints, strs)
+    return [
+        [
+            values[np.random.choice(len(values))][i]
+            for i in range(INPUT_LENGTH)
+        ]
+        for _ in range(INPUT_LENGTH)
+    ]
+
+
+@pytest.fixture
+def flat_dicts(nones, bools, floats, ints, strs):
+    np.random.seed(RANDOM_SEED)
+    values = (nones, bools, floats, ints, strs)
+    return [
+        {
+            strs[i]: values[np.random.choice(len(values))][i]
+            for i in range(INPUT_LENGTH)
+        }
+        for _ in range(INPUT_LENGTH)
+    ]
+
+
+@pytest.fixture
+def nested_lists(nones, bools, floats, ints, strs):
+    np.random.seed(RANDOM_SEED)
+    values = (nones, bools, floats, ints, strs)
+    flat_values = [value for type_values in values for value in type_values]
+    def gen_value(p=1):
+        if np.random.random() < p:
+            return [
+                gen_value(p/2)
+                for _ in range(np.random.choice(4))
+            ]
+        else:
+            return np.random.choice(flat_values)
+    return [
+        [
+            gen_value()
+            for _ in range(np.random.choice(3)+1)
+        ]
+        for _ in range(INPUT_LENGTH)
+    ]
+
+
+@pytest.fixture
+def nested_dicts(nones, bools, floats, ints, strs):
+    np.random.seed(RANDOM_SEED)
+    values = (nones, bools, floats, ints, strs)
+    flat_values = [value for type_values in values for value in type_values]
+    def gen_value(p=1):
+        if np.random.random() < p:
+            return {
+                key: gen_value(p/2)
+                for key, _ in zip(strs, range(np.random.choice(4)))
+            }
+        else:
+            return np.random.choice(flat_values)
+    return [
+        {
+            key: gen_value()
+            for key, _ in zip(strs, range(np.random.choice(3)+1))
+        }
+        for _ in range(INPUT_LENGTH)
+    ]
+
+
+@pytest.fixture
+def scalar_values(nones, bools, floats, ints, strs):
+    return [type_values[0]
+            for type_values in (nones, bools, floats, ints, strs)]
+
+
+@pytest.fixture
+def collection_values(flat_lists, flat_dicts, nested_lists, nested_dicts):
+    return [type_values[0]
+            for type_values in (flat_lists, flat_dicts, nested_lists, nested_dicts)]
+
+
+@pytest.fixture
+def all_values(scalar_values, collection_values):
+    return scalar_values + collection_values
 
 
 @pytest.fixture(scope='session')
