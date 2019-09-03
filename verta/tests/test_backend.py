321c946d1e1ba55eda174cc1d443aff92d5e4de2
Michael Liu <miliu@protonmail.com>
2019-09-03 14:43:44 -0700

    Reinstate back end load test

--- verta/tests/test_backend.py
+++ verta/tests/test_backend.py
@@ -3,40 +3,40 @@ import six
 from multiprocessing import Pool
 
 import pytest
-import utils
 
 
 class TestLoad:
     @staticmethod
-    def run_fake_experiment(client):
+    def run_fake_experiment(args):
+        client, floats = args
+        floats = iter(floats)
         run = client.set_experiment_run()
 
         run.log_attribute("is_test", True)
         run.get_attribute("is_test")
 
         run.log_hyperparameters({
-            'C': utils.gen_float(),
-            'solver': utils.gen_str(),
-            'max_iter': utils.gen_int(),
+            'C': next(floats),
+            'solver': next(floats),
+            'max_iter': next(floats),
         })
         run.get_hyperparameter("C")
         run.get_hyperparameter("solver")
         run.get_hyperparameter("max_iter")
 
-        run.log_observation("rand_val", utils.gen_float())
-        run.log_observation("rand_val", utils.gen_float())
-        run.log_observation("rand_val", utils.gen_float())
+        run.log_observation("rand_val", next(floats))
+        run.log_observation("rand_val", next(floats))
+        run.log_observation("rand_val", next(floats))
         run.get_observation("rand_val")
 
-        run.log_metric("val_acc", utils.gen_float())
+        run.log_metric("val_acc", next(floats))
         run.get_metric("val_acc")
 
         run.log_artifact("self", run)
         run.get_artifact("self")
 
-    @pytest.mark.skip(reason="there is an issue serializing the Client for Pool")
-    def test_load(self, client):
+    def test_load(self, client, floats):
         client.set_project()
         client.set_experiment()
         with Pool(36) as pool:
-            pool.map(self.run_fake_experiment, [client]*180)
+            pool.map(self.run_fake_experiment, [(client, floats)]*144)
