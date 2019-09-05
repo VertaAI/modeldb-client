2a6bd0110694a5f91fb97e938aa2a462fdbaeb8b
Michael Liu <miliu@protonmail.com>
2019-09-05 15:19:17 -0700

    Create fixture for log_model_for_depl ingredients

--- verta/tests/conftest.py
+++ verta/tests/conftest.py
@@ -1,12 +1,18 @@
 from __future__ import division
 
+import six
+
 import os
 import shutil
 import string
 
 from google.cloud import bigquery
 import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import linear_model
 
+import verta
 from verta import Client
 
 import pytest
@@ -249,3 +255,18 @@ def big_query_job():
     )
     job_id = query_job.job_id
     return (job_id, "US", query)
+
+@pytest.fixture
+def model_for_deployment(seed, strs):
+    num_rows, num_cols = 36, 6
+
+    model = sklearn.linear_model.LogisticRegression()
+    requirements = six.StringIO("scikit-learn=={}".format(sklearn.__version__))
+    data = pd.DataFrame(np.tile(np.arange(num_rows).reshape(-1, 1),
+                                num_cols),
+                        columns=strs[:num_cols])
+    X_train = data.iloc[:,:-1]  # pylint: disable=bad-whitespace
+    y_train = data.iloc[:, -1]
+    model_api = verta.utils.ModelAPI(X_train, y_train)
+
+    return model, model_api, requirements, X_train, y_train
