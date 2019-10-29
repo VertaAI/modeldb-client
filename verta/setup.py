d964d3dc806ba7ff8846b1d3a99735b6492d9e8d
Michael Liu <miliu@protonmail.com>
2019-10-29 13:51:37 -0700

    Bump pathlib2 ver to match pytest

--- verta/setup.py
+++ verta/setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "cloudpickle",
         "googleapis-common-protos>=1.5",
-        "pathlib2>=2.1",
+        "pathlib2>=2.2",
         "protobuf>=3.6",
         "requests>=2.21, <3.0",
         "six>=1.12",
