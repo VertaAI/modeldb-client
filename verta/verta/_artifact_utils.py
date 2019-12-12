d22e4833e6f20cf233f51ecf817db20b09736a39
Michael Liu <miliu@protonmail.com>
2019-12-12 10:51:44 -0800

    Support tensorflow-hub in process_reqs()

--- verta/verta/_artifact_utils.py
+++ verta/verta/_artifact_utils.py
@@ -31,10 +31,12 @@ except ImportError:  # TensorFlow not installed
 PYPI_TO_IMPORT = {
     'scikit-learn': "sklearn",
     'tensorflow-gpu': "tensorflow",
+    'tensorflow-hub': "tensorflow_hub",
     'beautifulsoup4': "bs4",
 }
 IMPORT_TO_PYPI = {  # separate mapping because PyPI to import is surjective
     'sklearn': "scikit-learn",
+    'tensorflow_hub': "tensorflow-hub",
     'bs4': "beautifulsoup4",
 }
 REQ_SPEC_REGEX = re.compile(r"([a-zA-Z0-9._-]+)(.*)?")  # https://www.python.org/dev/peps/pep-0508/#names
