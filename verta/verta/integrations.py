3f052bb902c97a18b80f515bff2354e578007037
Michael Liu <miliu@protonmail.com>
2020-01-15 15:19:53 -0800

    More enhancements

--- verta/verta/integrations.py
+++ verta/verta/integrations.py
@@ -28,10 +28,19 @@ if tensorflow is not None:
         run : :class:`~verta.client.ExperimentRun`
             Experiment Run tracking this model.
 
+        Examples
+        --------
+        >>> run = client.set_experiment_run()
+        >>> model.fit(
+        ...     X_train, y_train,
+        ...     callbacks=[verta.integrations.KerasCallback(run)],
+        ... )
+
         """
         def __init__(self, run):
             self.run = run
 
-        def on_epoch_end(self, epoch, logs={}):
-            for key, val in _six.viewitems(logs):
-                self.run.log_observation(key, float(val))
+        def on_epoch_end(self, epoch, logs=None):
+            if logs is not None:
+                for key, val in _six.viewitems(logs):
+                    self.run.log_observation(key, float(val))
