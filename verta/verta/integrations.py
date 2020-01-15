a2640f85c53918437a7fb438ef77d365dfb9148c
Michael Liu <miliu@protonmail.com>
2020-01-15 15:33:36 -0800

    Log training hyperparameters

--- verta/verta/integrations.py
+++ verta/verta/integrations.py
@@ -9,7 +9,6 @@ import os
 import requests
 
 from . import _utils
-from . import utils
 
 
 try:
@@ -31,6 +30,11 @@ if tensorflow is not None:
         Examples
         --------
         >>> run = client.set_experiment_run()
+        >>> model.compile(
+        ...     loss="categorical_crossentropy",
+        ...     optimizer="adam",
+        ...     metrics=["accuracy"],
+        ... )
         >>> model.fit(
         ...     X_train, y_train,
         ...     callbacks=[verta.integrations.KerasCallback(run)],
@@ -40,7 +44,21 @@ if tensorflow is not None:
         def __init__(self, run):
             self.run = run
 
+        def set_params(self, params):
+            if isinstance(params, dict):
+                for key, val in _six.viewitems(params):
+                    try:
+                        self.run.log_hyperparameter(key, _utils.to_builtin(val))
+                    except (ValueError,  # key already logged
+                            TypeError):  # unloggable type
+                        pass
+                    except:  # don't halt execution
+                        pass
+
+        def set_model(self, model):
+            pass
+
         def on_epoch_end(self, epoch, logs=None):
-            if logs is not None:
+            if isinstance(logs, dict):
                 for key, val in _six.viewitems(logs):
-                    self.run.log_observation(key, float(val))
+                    self.run.log_observation(key, _utils.to_builtin(val))
