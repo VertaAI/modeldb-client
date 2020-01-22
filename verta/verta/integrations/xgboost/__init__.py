ce8e17e4101c3d8f9136b6677ec6a28e03dea0ec
Michael Liu <miliu@protonmail.com>
2020-01-22 14:36:23 -0800

    Impl XGBoost callback

---
+++ verta/verta/integrations/xgboost/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from ... import _six
+
+import xgboost as xgb
+
+from ... import _utils
+
+
+def verta_callback(run):
+    """
+    XGBoost callback that automates logging to Verta during booster training.
+
+    .. versionadded:: 0.13.20
+
+    Parameters
+    ----------
+    run : :class:`~verta.client.ExperimentRun`
+        Experiment Run tracking this model.
+
+    Examples
+    --------
+    >>> from verta.integrations.xgboost import verta_callback
+    >>> run = client.set_experiment_run()
+    >>> run.log_hyperparameters(params)
+    >>> bst = xgb.train(
+    ...     params, dtrain,
+    ...     evals=[(dtrain, "train")],
+    ...     callbacks=[verta_callback(run)],
+    ... )
+
+    """
+    def callback(env):
+        print(env.evaluation_result_list)
+        for metric, val in env.evaluation_result_list:
+            run.log_observation(metric, val)
+        # TODO: support `xgb.cv()`, which gives `(metric, val, std_dev)` across folds
+    return callback
